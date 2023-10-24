459535e2fb3eb0faa9eebac23a93f235ec1cf415
hw1010 <hyewon6588@naver.com>
2023-10-24 13:51:00 -0400

    first commit

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Baquibolle");
+        System.out.println(12);//int
+        System.out.println(-34);//int
+        System.out.println(3.14);//double
+        System.out.println(true);//boolean
+        System.out.println(false);
+    }
+}
