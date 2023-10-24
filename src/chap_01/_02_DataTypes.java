84bfcb7b7c205486a5f24babbb4b8c2fc4df3e59
hw1010 <hyewon6588@naver.com>
2023-10-24 15:30:21 -0400

    second commit

--- src/chap_01/_02_DataTypes.java
+++ src/chap_01/_02_DataTypes.java
@@ -9,5 +9,7 @@ public class _02_DataTypes {
         System.out.println(3.14);//double
         System.out.println(true);//boolean
         System.out.println(false);
+
+        System.out.println(123+345);
     }
 }
